eea96e9e51ca44cd0f110f2b392dfc0c6c9cb95a
boob-eng <glasikov@inbox.ru>
2025-12-21 22:21:38 +0300

    Create number1.java

---
+++ number 1/number1.java
@@ -0,0 +1,28 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+public class n1
+{
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = System.out;
+    public static int Sum(int a)
+    {
+        int s=0;
+        while (a>0){
+            s+=a%10;
+            a/=10;
+        }
+        return s;
+    }
+    public static void main(String[] args)
+    {
+        int a0=Math.abs(in.nextInt());
+        int n=in.nextInt();
+        for (int i=0;i<n;i++){
+            a0=2*Sum(a0);
+            out.print(a0);
+            if (i<n-1){
+                out.print(", ");
+            }
+        }
+    }
+}
